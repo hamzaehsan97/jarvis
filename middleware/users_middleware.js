7abc84dc449b5838f0e28d2857e507a1e2adf66d
hamzaehsan97 <hamzaehsan75@gmail.com>
2022-12-27 16:21:37 -0800

    remove bad logging

--- middleware/users_middleware.js
+++ middleware/users_middleware.js
@@ -2,9 +2,6 @@
 
 // create user middleware
 exports.createUser = (req, res, next) => {
-  console.log("email in users middleware", req.query.email);
-  console.log("password in users middleware", req.query.password);
-
   if (req.method == "POST") {
     if (req.query.email == null || req.query.password == null) {
       throw new Error("Validation error: email or password cannot be null.");
