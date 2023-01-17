3d5c68a5fa1c504f814925342a9868ff8518f7ed
hamzaehsan97 <hamzaehsan75@gmail.com>
2023-01-16 17:49:33 -0800

    passwords service

--- constants/service_constants.js
+++ constants/service_constants.js
@@ -1,6 +1,7 @@
-exports.services = ["notes", "finance", "health"];
+exports.services = ["notes", "finance", "health", "passwords"];
 exports.nested_services = {
   notes: { activated: false },
   finance: { activated: false },
   health: { activated: false },
+  passwords: { activated: false },
 };
