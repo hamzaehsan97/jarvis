ce534d2dc83e82d5fcae99f86a92e7cbd35f41e1
Hamza Ehsan <hamzaehsan75@gmail.com>
2023-10-02 17:42:36 -0700

    Fix report generation time diff

--- routes/finance.js
+++ routes/finance.js
@@ -212,7 +212,7 @@ const get_items = async function (request, response, args) {
   // If the last time the report was run was more than 6 days, generate a new report for the user and send a message asking to refresh page.
   let message = null;
   if (results.length > 0 && body.item_type === "finance_report") {
-    let timeDiff = Date.now() - results[-1].creationTime.timestamp;
+    let timeDiff = Date.now() - results[results.length - 1].creationTime.timestamp;
     timeDiff = Math.floor(timeDiff / 1000 / 60 / 60 / 24);
     console.log("Time diff in days", timeDiff);
     if (timeDiff > 6) {
