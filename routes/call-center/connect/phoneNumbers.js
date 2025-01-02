4b283c2daa173df4b87acad77c877f2480b364af
hamzaehsan97 <hamzaehsan75@gmail.com>
2025-01-01 20:35:47 -0800

    feat: Associate phone number with contact flow

--- routes/call-center/connect/phoneNumbers.js
+++ routes/call-center/connect/phoneNumbers.js
@@ -75,31 +75,72 @@ exports.claim_phone_number = function(req,res,next){
     });
 }
 
-exports.associate_phone_number = function(req,res,next){
-
+exports.associate_phone_number = function (req, res, next) {
+    const flows_table_name = "Flows";
     const contact_flow_id = req.body.contact_flow_id;
     const phone_number_id = req.body.phone_number_id;
+    const phone_number = req.body.phone_number;
 
     AWS.config.update({ region: "us-west-2" });
-    var connectClient = new AWS.Connect();
+    const connectClient = new AWS.Connect();
 
-    var associate_phone_number_params = {
+    const associate_phone_number_params = {
         InstanceId: req.instanceID,
         ContactFlowId: contact_flow_id,
-        PhoneNumberId: phone_number_id
-    }
+        PhoneNumberId: phone_number_id,
+    };
 
-    logger.log("associating phone number to flow for customers: "+req.email, req);
+    logger.log("Associating phone number to flow for customer: " + req.email, req);
 
-    connectClient.associatePhoneNumberContactFlow(associate_phone_number_params, function(err, data){
-        if(err){
-            logger.logError("Unable to associate phone number to flow for customer"+req.email, err, req);
+    connectClient.associatePhoneNumberContactFlow(associate_phone_number_params, function (err, data) {
+        if (err) {
+            logger.logError("Unable to associate phone number to flow for customer: " + req.email, err, req);
             next(err.message);
-        }else{
-            res.send(data);
+        } else {
+            console.log("Updating Flows table with phone number" + phone_number + " for flow ID: " + contact_flow_id);
+            // Add phone number ID to flows DDB table for flow ID
+            const params = {
+                TableName: flows_table_name,
+                Key: {
+                    flowOwner: { S: req.email },
+                    flowID: { S: contact_flow_id },
+                },
+                UpdateExpression: "SET #phoneNumber = :phoneNumberValue",
+                ExpressionAttributeNames: {
+                    "#phoneNumber": "phoneNumber", // Use a placeholder for the attribute name
+                },
+                ExpressionAttributeValues: {
+                    ":phoneNumberValue": { S: phone_number }, // Use a placeholder for the value
+                },
+                ReturnValues: "UPDATED_NEW", // Return the newly updated attributes
+            };
+
+            const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+            ddb.updateItem(params, function (err, data) {
+                if (err) {
+                    logger.logError(
+                        "Failed to update flow for customer: " +
+                            req.email +
+                            " with flowID: " +
+                            contact_flow_id,
+                        err,
+                        req
+                    );
+                    next(err.message);
+                } else {
+                    logger.log(
+                        "Successfully updated flow for customer: " +
+                            req.email +
+                            " with flowID: " +
+                            contact_flow_id,
+                        req
+                    );
+                    res.send(data);
+                }
+            });
         }
     });
-}
+};
 
 exports.describe_phone_number = function(req,res,next){
 
